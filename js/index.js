7fe11e7c66bdf89c89a4c50e3f5107544f085dc6
HugoZak <zakrajsekhugo@gmail.com>
2023-09-20 16:58:54 +0200

    add page connexion

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function switchCard() {
+    const loginCard = document.querySelector('.container .card:nth-child(2)');
+    const registerCard = document.querySelector('.container .card:nth-child(3)');
+    const registerButton = document.querySelector('.container .bouton-switch:nth-child(5)')
+    const loginButton = document.querySelector('.container .bouton-switch:nth-child(6)')
+  
+    if (loginCard.style.display === 'none') {
+      loginCard.style.display = 'block';
+      registerCard.style.display = 'none';
+      loginButton.style.display = 'none';
+      registerButton.style.display = 'block';
+    } else {
+      loginCard.style.display = 'none';
+      registerCard.style.display = 'block';
+      loginButton.style.display = 'block';
+      loginButton.style.width = '20%'
+      registerButton.style.display = 'none';
+    }
+  }
+  
