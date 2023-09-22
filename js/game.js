41ff67d4b6c36186971ee004e9d8ce83a3c7a6f1
pierreBRO <pierre.brossard@my-digital-school.org>
2023-09-22 12:07:21 +0200

    add queen

--- js/game.js
+++ js/game.js
@@ -1,52 +1,52 @@
-function game(){
-    let artiste = 'queen';
-    let titre = '';
-    let artisteGuess = document.getElementById('artiste').value;
-    let titreGuess = document.getElementById('titre').value;
+function game() {
+  let artiste = "queen";
+  let titre = "bohemian rhapsody";
+  let artisteGuess = document.getElementById("artiste").value;
+  let titreGuess = document.getElementById("titre").value;
 
-    if(artiste===artisteGuess && titre===titreGuess){
-        showSuccessMessage();
-    } else if(artiste===artisteGuess){
-        showArtisteMessage();
-    } else if (titre===titreGuess){
-        showTitreMessage();
-    } else {
-        showPerduMessage();
-    }
+  if (artiste === artisteGuess && titre === titreGuess) {
+    showSuccessMessage();
+  } else if (artiste === artisteGuess) {
+    showArtisteMessage();
+  } else if (titre === titreGuess) {
+    showTitreMessage();
+  } else {
+    showPerduMessage();
+  }
 }
 
 function showSuccessMessage() {
-    const successMessage = document.querySelector(".gagne-message");
+  const successMessage = document.querySelector(".gagne-message");
 
-    successMessage.style.display = "block";
-    setTimeout(function() {
-      successMessage.style.display = "none";
-    }, 5000); 
+  successMessage.style.display = "block";
+  setTimeout(function () {
+    successMessage.style.display = "none";
+  }, 5000);
 }
 
 function showArtisteMessage() {
-    const artisteMessage = document.querySelector(".presque-artiste");
+  const artisteMessage = document.querySelector(".presque-artiste");
 
-    artisteMessage.style.display = "block";
-    setTimeout(function() {
-        artisteMessage.style.display = "none";
-    }, 5000); 
+  artisteMessage.style.display = "block";
+  setTimeout(function () {
+    artisteMessage.style.display = "none";
+  }, 5000);
 }
 
 function showTitreMessage() {
-    const titreMessage = document.querySelector(".presque-titre");
+  const titreMessage = document.querySelector(".presque-titre");
 
-    titreMessage.style.display = "block";
-    setTimeout(function() {
-        titreMessage.style.display = "none";
-    }, 5000); 
+  titreMessage.style.display = "block";
+  setTimeout(function () {
+    titreMessage.style.display = "none";
+  }, 5000);
 }
 
 function showPerduMessage() {
-    const perduMessage = document.querySelector(".perdu-message");
+  const perduMessage = document.querySelector(".perdu-message");
 
-    perduMessage.style.display = "block";
-    setTimeout(function() {
-        perduMessage.style.display = "none";
-    }, 5000); 
+  perduMessage.style.display = "block";
+  setTimeout(function () {
+    perduMessage.style.display = "none";
+  }, 5000);
 }
